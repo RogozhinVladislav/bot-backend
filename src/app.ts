ff2c0018d23381f3f8cc3a858a15944c8e08f29c
RogozhinVladislav <rogojinvladislav@gmail.com>
2020-01-11 22:21:19 +0500

    deploy

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,26 @@ const { PORT } = process.env
 import initBot from './helpers/init-bot'
 import vk from './helpers/vk'
 
-mongoose()
-  .then((info:any) => {
-    console.log(`Connected to ${info.host}:${info.port}/${info.name}`)
-    app.listen(PORT, () => {
-      console.log(`Listenning port: ${PORT}`)
-    })
-    initBot()
-    vk.updates.start().catch(console.error)
-  })
-  .catch(() => {
-    console.error('error')
-    process.exit(1)
-  })
+app.listen(PORT, () => {
+  console.log(`Listenning port: ${PORT}`)
+})
+
+vk.updates.hear('hi', async context => {
+  context.send('hellozxx')
+});
+
+vk.updates.start().catch(console.error)
+
+// mongoose()
+//   .then((info:any) => {
+//     console.log(`Connected to ${info.host}:${info.port}/${info.name}`)
+//     app.listen(PORT, () => {
+//       console.log(`Listenning port: ${PORT}`)
+//     })
+//     initBot()
+//     vk.updates.start().catch(console.error)
+//   })
+//   .catch(() => {
+//     console.error('error')
+//     process.exit(1)
+//   })
